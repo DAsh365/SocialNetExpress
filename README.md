6a587ac1a121b4a33b7198438d2f4a9492e9db79
Devan Ash <141689897+DAsh365@users.noreply.github.com>
2024-02-18 20:51:05 -0700

    updating files

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # SocialNetExpress
+
+## Description
+
+This repository contains a social network API built using Express.js and MongoDB with Mongoose. The API allows users to share their thoughts, react to friends' thoughts, and manage their friend list.
+
+## Installation
+
+1. Make sure you have MongoDB installed on your machine. Follow the [MongoDB installation guide](https://docs.mongodb.com/manual/installation/) if you haven't already.
+2. Clone this repository to your local machine.
+3. Navigate to the project directory in your terminal.
+4. Run `npm install` to install dependencies.
+
+## Usage
+
+1. Start the server by running `npm start`.
+2. Use Insomnia or any other REST client to test the API endpoints.
+
+## API Endpoints
+
+### Users
+
+- `GET /api/users`: Get all users.
+- `GET /api/users/:userId`: Get a single user by ID.
+- `POST /api/users`: Create a new user.
+- `PUT /api/users/:userId`: Update a user by ID.
+- `DELETE /api/users/:userId`: Delete a user by ID.
+
+### Friends
+
+- `POST /api/users/:userId/friends/:friendId`: Add a friend to a user's friend list.
+- `DELETE /api/users/:userId/friends/:friendId`: Remove a friend from a user's friend list.
+
+### Thoughts
+
+- `GET /api/thoughts`: Get all thoughts.
+- `GET /api/thoughts/:thoughtId`: Get a single thought by ID.
+- `POST /api/thoughts`: Create a new thought.
+- `PUT /api/thoughts/:thoughtId`: Update a thought by ID.
+- `DELETE /api/thoughts/:thoughtId`: Delete a thought by ID.
+
+### Reactions
+
+- `POST /api/thoughts/:thoughtId/reactions`: Add a reaction to a thought.
+- `DELETE /api/thoughts/:thoughtId/reactions/:reactionId`: Remove a reaction from a thought.
+
+## Walkthrough Video
+
+[Link to Walkthrough Video](add_link_here)
+
+## Bonus
+
+The application automatically deletes a user's associated thoughts when the user is deleted.
+
+## Credits
+
+This project is part of the curriculum of [edX Boot Camps LLC](https://www.edx.org/bootcamps).
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ controllers/thoughtController.js
@@ -0,0 +1,127 @@
+const { Thought, User } = require("../models");
+
+module.exports = {
+  async getThoughts(req, res) {
+    try {
+      const dbThoughtData = await Thought.find();
+      res.json(dbThoughtData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async getSingleThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOne({
+        _id: req.params.thoughtId,
+      });
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: "Thought doesn't exist" });
+      }
+      res.json(dbThoughtData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async createThought(req, res) {
+    console.log(req.body)
+    try {
+      const dbThoughtData = await Thought.create(req.body);
+      const dbUserData = await User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $addToSet: { thoughts: dbThoughtData._id } },
+        { new: true }
+      );
+      if (!dbUserData) {
+        return res
+          .status(404)
+          .json({ message: "Thought created, but no user found" });
+      }
+      res.json(dbThoughtData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async updateThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndUpdate(
+        {
+          _id: req.params.thoughtId,
+        },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: "Thought ID doesn't exist" });
+      }
+      res.json({ message: "Updated Thought" });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async deleteThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndRemove({
+        _id: req.params.thoughtId,
+      });
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: "Thought ID doesn't exist" });
+      }
+      const dbUserData = await User.findOneAndUpdate(
+        { _id: dbThoughtData.username },
+        { $pull: { thoughts: req.params.thoughtId } },
+        { new: true }
+      );
+      if (!dbUserData) {
+        return res
+          .status(404)
+          .json({ message: "Thought deleted but no user has this ID" });
+      }
+      res.status(200).json({ message: "Thought Deleted" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  async addReaction(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndUpdate({
+        _id: req.params.thoughtId,
+      },
+        { $addToSet: { reactions: req.body } },
+        {
+          runValidators: true,
+          new: true
+        }
+      );
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: "Thought ID does not exist" });
+      }
+
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  },
+  
+  async deleteReaction(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { new: true }
+      );
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: "Thought ID doesn't exist" });
+      }
+      res.json({ message: "Reaction deleted" });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
